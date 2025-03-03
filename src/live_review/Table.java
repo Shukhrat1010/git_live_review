a14904707f2df9bda5793e2c19f94343bb0572aa
shukhrat1010 <snurmahmedov88@mail.com>
2025-03-03 14:06:31 +0500

    Overloaded Method: multiplicationTable method to print table 1-10

--- src/live_review/Table.java
+++ src/live_review/Table.java
@@ -3,10 +3,12 @@ package live_review;
 public class Table {
     public static void main(String[] args) {
         multiplicationTable(5);
+        multiplicationTable();
     }
 
         /*
-    Method: multiplicationTable
+    Overloaded Method: multiplicationTable
+    param: num
     Return: void
     prints multiplication table for the numbers 1 - 10
      */
@@ -14,6 +16,18 @@ public class Table {
     public static void multiplicationTable(int num) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(num + "x" + i + "=" + num * i);
+        }
+    }
+
+        /*
+    Method: multiplicationTable
+    Return: void
+    prints multiplication table for the numbers 1 - 9
+     */
+    public static void multiplicationTable(){
+        for (int i = 1; i <= 10; i++) {
+            multiplicationTable(i);
+            System.out.println("-----------");
         }
     }
 }
